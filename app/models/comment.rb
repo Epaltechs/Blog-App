b9cfc70a9f4f8c49f2684001f17b77b78e2eafa2
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-08-31 21:34:31 +0100

    Create view and add user data

---
+++ app/models/comment.rb
@@ -0,0 +1,12 @@
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+  after_save :update_comments_counter
+
+  private
+  #   A method that updates the comments counter for a user.
+
+  def update_comments_counter
+    post.increment(:comments_counter)
+  end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,18 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: :destroy, foreign_key: 'post_id'
+  has_many :likes, dependent: :destroy, foreign_key: 'post_id'
+  validates :title, presence: true, length: { maximum: 250, too_long: 'Name must not exceed 250 characters' }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  #   A method that updates the posts counter for a user.
+
+  def update_posts_counter
+    user.increment!(:posts_counter)
+  end
+
+  def recent_five_comments()
+    comments.order(created_at: :desc).limit(5)
+  end
+end
