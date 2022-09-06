2e086f8cdc0414d5434308ae1c1b7b9712c2d857
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-09-06 16:37:35 +0300

    Add user index page posts counter test

--- spec/integration/user_index_spec.rb
+++ spec/integration/user_index_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :system do
   before do
     @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                              bio: 'Teacher from Mexico.', posts_counter: 1)
-    @second_user = User.create(name: 'Lily', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland',
-                               posts_counter: 2)
+                              bio: 'Teacher from Mexico.')
+    @second_user = User.create(name: 'Lily', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                               bio: 'Teacher from Poland')
   end
   describe 'index page' do
     it 'shows correct username' do
@@ -20,14 +20,19 @@ RSpec.describe 'Users', type: :system do
       expect(page).to have_selector("img[src*='#{@first_user.photo}']")
     end
 
-    # it 'shows the number of posts each user has written' do
-    #   expect(page).to have_content(Post.all.count)
-    # end
+    it 'shows the number of posts each user has writtenr' do
+      Post.create(title: 'Hello', text: 'This is my first post', user: @first_user)
+      Post.create(title: 'Hello2', text: 'This is my second post', user: @first_user)
+      visit users_path
+      expect(page).to have_content("Number of posts: #{@first_user.posts_counter}")
+    end
+
     # it 'when user is clicked, it should be redirected to user show page' do
-    #   User.all.each do |i|
-    #     click_link i.name
-    #     expect(page).to have_current_path("/users/#{i.id}")
-    #   end
+    #   third_user = User.create(name: 'Michael', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+    #                            bio: 'Teacher from Poland', posts_counter: 2)
+    #   visit users_path
+    #   click_link third_user
+    #   expect(page).to have_current_path(user_path(third_user.id))
     # end
   end
 end
