39aca20c05a7cc10c0c0841c889d01477f271044
Emmanuel Paul <91189703+Epaltechs@users.noreply.github.com>
2022-09-07 10:40:39 +0100

    Edit controller actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,16 +11,22 @@ class CommentsController < ApplicationController
     @new_comment.post = @post
 
     if @new_comment.save
+      flash[:notice] = 'Your comment created successfully'
       @new_comment.update_comment_counter
       redirect_to user_post_path(current_user.id, @post.id)
     else
-      render :new, alert: 'An error occurred while creating the post'
+      render :new, alert: 'An error occurred while creating the comment'
     end
   end
 
-  private
-
-  def comment_params
-    params.require(:comment).permit(:text)
+  def destroy
+    deleted_comment = Comment.find(params[:id])
+    post = Post.find(params[:post_id])
+    post.comments_counter -= 1
+    deleted_comment.destroy
+    if post.save
+      redirect_to user_post_path(params[:user_id], post.id), notice: 'You have deleted the comment successfully!'
+    else
+      render :new, alert: 'An error has occurred while deleting the comment'
+    end
   end
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,10 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @users = User.all
-    @posts = Post.all.length
   end
 
   def show
     @user = User.find(params[:id])
-    @posts = Post.all
-    @comments = Comment.where(post_id: 2)
-    @likes = Like.all
   end
 end
